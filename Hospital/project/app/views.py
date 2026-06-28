f62563b277ec71cf911cb27b4fbddb23d2cd09e1
abhishek kumar <abhishek756491@gmail.com>
2026-06-28 20:54:18 +0530

    update

---
+++ Hospital/project/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def base(request):
+    return render(request,'base.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
