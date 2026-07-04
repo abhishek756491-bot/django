25361e6fb1479a88ca83cb985f6d0f291d363e0e
abhishek kumar <abhishek756491@gmail.com>
2026-07-04 11:22:46 +0530

    update

--- Hospital/project/app/models.py
+++ Hospital/project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from app.models import *
 
 # Create your models here.
+
+# class doctors(models.Model):
+#     name=models.()
+
+
--- Hospital/project/project/urls.py
+++ Hospital/project/project/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('home',views.home,name='home'),
     path('base',views.base,name='base'),
     path('about',views.about,name='about'),
-    path('',views.services,name='services')
+    path('services',views.services,name='services')
 ]
