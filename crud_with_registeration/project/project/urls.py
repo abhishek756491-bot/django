79e809a88a0ef9ba6dc858113697ac777a05cf17
abhishek kumar <abhishek756491@gmail.com>
2026-07-11 16:05:15 +0530

    update

--- crud_with_registeration/project/project/urls.py
+++ crud_with_registeration/project/project/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
     path('login/',login,name='login'),
+    path('query/',query,name='query'),
+    path('edit/<int:pk>/',edit,name='edit'),
+    path('update/<int:pk>/',update,name='update'),
 ]
