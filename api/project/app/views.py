373798cc0f0dfebe22e50afc2d46a0ba45425774
abhishek kumar <abhishek756491@gmail.com>
2026-07-14 16:04:34 +0530

    update

--- api/project/app/views.py
+++ api/project/app/views.py
@@ -1,3 +1,71 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
 
-# Create your views here.
+
+# GET ALL + POST
+
+@api_view(['GET', 'POST'])
+def student_api(request):
+    # GET DATA
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+    
+    # CREATE DATA
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': 'Student Added',
+                'data': serializer.data
+
+            })
+
+        return Response(serializer.errors)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def student_detail(request, id):
+
+    student = Student.objects.get(id=id)
+
+    # SINGLE GET
+    if request.method == 'GET':
+
+        serializer = StudentSerializer(student)
+
+        return Response(serializer.data)
+
+
+    # UPDATE
+    elif request.method == 'PUT':
+
+        serializer = StudentSerializer(student,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': 'Student Updated',
+                'data': serializer.data
+            })
+        return Response(serializer.errors)
+
+
+    # DELETE
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response({
+            'message': 'Student Deleted'
+        })
+    
+
+
+# {
+#    "name":"Mehak",
+#    "course":"AI",
+#    "email":"mehak@gmail.com"
+# }
--- api/project/project/urls.py
+++ api/project/project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/students', student_api),
+    path('students/<int:id>/', student_detail)
 ]
